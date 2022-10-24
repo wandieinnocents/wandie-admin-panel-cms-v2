66b72121409f624c4b2942326b73d74974b58338
wandieinnocents <wandieinnocent2@gmail.com>
2022-10-24 23:48:35 +0300

    Refactor code

--- app/Http/Controllers/Api/v1/Admin/AdminApiVideoController.php
+++ app/Http/Controllers/Api/v1/Admin/AdminApiVideoController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Http\Controllers\Api\v1\Admin;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\VideoGallery;
-
 class AdminApiVideoController extends Controller
 {
     /**
@@ -17,11 +14,9 @@ class AdminApiVideoController extends Controller
     {
         $videos = VideoGallery::all();
         $count_videos = VideoGallery::count();
-
         if(VideoGallery::count() > 0){
             $videos = VideoGallery::all();
             $count_videos = VideoGallery::count();
-    
             // return
             return [
                 "status" => 200,
@@ -31,7 +26,6 @@ class AdminApiVideoController extends Controller
             ];
         }
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -41,7 +35,6 @@ class AdminApiVideoController extends Controller
     {
         //
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -62,9 +55,7 @@ class AdminApiVideoController extends Controller
             $file->move('uploads/video_thumbnails/',$filename);
             $video->video_thumbnail   = url('uploads' . '/video_thumbnails/'  . $filename);
         }
-
         $video->save();
-
          // response
          return [
             "status" => 200,
@@ -72,7 +63,6 @@ class AdminApiVideoController extends Controller
             "data" => $video
         ];
     }
-
     /**
      * Display the specified resource.
      *
@@ -84,7 +74,6 @@ class AdminApiVideoController extends Controller
         // find video id
         if(VideoGallery::where("id", $id)->exists()){
         $video = VideoGallery::find($id);
-
         // return response
         return [
             "status" => 200,
@@ -92,19 +81,14 @@ class AdminApiVideoController extends Controller
             "data" =>$video
         ];
     }
-
      // if no record
      else { 
-
         return [
             "status" => 404,
             "message" => "Oops!, No video Found ",
-           
         ];
-    
     }
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -115,7 +99,6 @@ class AdminApiVideoController extends Controller
     {
         //
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -130,7 +113,6 @@ class AdminApiVideoController extends Controller
             $video->video_title = !empty($request->video_title)? $request->video_title: $video->video_title;
             $video->video_url = !empty($request->video_url)? $request->video_url: $video->video_url;
             $video->video_description = !empty($request->video_description)? $request->video_description: $video->video_description;
-
             // video thumbnail
             if($request->hasfile('video_thumbnail')){
                 $file               = $request->file('video_thumbnail');
@@ -139,31 +121,24 @@ class AdminApiVideoController extends Controller
                 $file->move('uploads/video_thumbnails/',$filename);
                 $video_update->video_thumbnail   = url('uploads' . '/video_thumbnails/'  . $filename);
             }
-
             // save video 
             $video->save();
-    
             // response for success
             return [
                 "status" => 200,
                 "message" => "Video updated successfully",
                 "data" => $video
-               
             ];
         }
         // if no record by id found
         else { 
-    
             // response for success
             return [
                 "status" => 404,
                 "message" => "Oops!, No Video Found ",
-               
             ];
-        
         }
     }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -183,19 +158,13 @@ class AdminApiVideoController extends Controller
             "data" => $video,
         ];
     }  
-    
     // if no record
     else { 
         // response for success
         return [
             "status" => 404,
             "message" => "Oops!, No Video Found to Delete "
-            
-           
         ];
-    
     }
-
-    
     }
 }
