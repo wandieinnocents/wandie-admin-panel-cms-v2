5b8ce5d92c7e8d1f39c9e6db517ad3a4cbf8c4cc
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-14 23:58:44 +0300

    Add disquscomments

--- resources/views/backend/pages_backend/posts/index.blade.php
+++ resources/views/backend/pages_backend/posts/index.blade.php
@@ -168,14 +168,41 @@ Posts
                                     <p>Description : {{ $post->post_description }}</p>
 
                                     <!-- post comment box here -->
-                                    <form>
+                                    <!-- diqus -->
+                                    <div id="disqus_thread"></div>
+                                        <script>
+                                            /**
+                                            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                                            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+                                            /*
+                                            var disqus_config = function () {
+                                            this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                                            this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                                            };
+                                            */
+                                            (function() { // DON'T EDIT BELOW THIS LINE
+                                            var d = document, s = d.createElement('script');
+                                            s.src = 'https://website-yk9vnplwgy.disqus.com/embed.js';
+                                            s.setAttribute('data-timestamp', +new Date());
+                                            (d.head || d.body).appendChild(s);
+                                            })();
+                                        </script>
+                                     <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+
+                                    <!-- end of disqus -->
+                                    <!-- <form>
                                     <div class="mb-3">
                                                 <label for="message-text" class="col-form-label">Add Comment:</label>
                                                 <textarea class="form-control" name="post_description" value="" id="message-text">{{  $post->post_description }}</textarea>
                                       </div>
-                                    </form>
+                                    </form> -->
 
                                     </div>
+
+
+
+                                    <!-- modal footer -->
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close </button>
                                         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editPostOrder{{ $post->id }}" data-bs-whatever="@getbootstrap">Edit post</button>
@@ -280,3 +307,4 @@ Posts
 
 
 @endsection
+
