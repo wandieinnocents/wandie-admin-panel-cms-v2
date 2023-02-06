cc2bed61b0a5b6d022f3fc6909a102ada0792508
wandieinnocents <wandieinnocent2@gmail.com>
2023-02-06 23:06:31 +0300

    Refactor code

--- database/migrations/2023_01_28_204531_create_products_table.php
+++ database/migrations/2023_01_28_204531_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('');
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
         });
     }
