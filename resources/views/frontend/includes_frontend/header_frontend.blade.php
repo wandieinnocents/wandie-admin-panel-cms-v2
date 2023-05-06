bd1be0d056b4929b18bfdfc4d25aea4e8e5f619c
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-07 00:51:29 +0300

    modify menu

--- resources/views/frontend/includes_frontend/header_frontend.blade.php
+++ resources/views/frontend/includes_frontend/header_frontend.blade.php
@@ -282,38 +282,8 @@
                                     <li class="active"><a  href="/"><i class="zmdi zmdi-home"></i> home </a>
 
                                     </li>
-                                    <li class="mega_items"><a href="shop.html"><i class="zmdi zmdi-shopping-basket"></i> shop <i class="zmdi zmdi-caret-down"></i></a>
-                                        <div class="mega_menu">
-                                            <ul class="mega_menu_inner">
-                                                <li><a href="#">Shop Layouts</a>
-                                                    <ul>
-                                                        <li><a href="shop-fullwidth.html">Full Width</a></li>
-                                                        <li><a href="shop-fullwidth-list.html">Full Width list</a></li>
-                                                        <li><a href="shop-right-sidebar.html">Right Sidebar </a></li>
-                                                        <li><a href="shop-right-sidebar-list.html"> Right Sidebar list</a></li>
-                                                        <li><a href="shop-list.html">List View</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#">other Pages</a>
-                                                    <ul>
-                                                        <li><a href="cart.html">cart</a></li>
-                                                        <li><a href="wishlist.html">Wishlist</a></li>
-                                                        <li><a href="checkout.html">Checkout</a></li>
-                                                        <li><a href="my-account.html">my account</a></li>
-                                                        <li><a href="404.html">Error 404</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#">Product Types</a>
-                                                    <ul>
-                                                        <li><a href="product-details.html">product details</a></li>
-                                                        <li><a href="product-sidebar.html">product sidebar</a></li>
-                                                        <li><a href="product-grouped.html">product grouped</a></li>
-                                                        <li><a href="variable-product.html">product variable</a></li>
+                                    <li class="mega_items"><a href="shop.html"><i class="zmdi zmdi-shopping-basket"></i> shop </a>
 
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </div>
                                     </li>
                                     <li><a href="blog.html"><i class="zmdi zmdi-collection-music"></i> blog <i class="zmdi zmdi-caret-down"></i></a>
                                         <ul class="sub_menu pages">
