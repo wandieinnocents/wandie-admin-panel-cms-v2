4b832f528e3c88253235fdc44598dffc9c48fbfe
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-07 00:32:26 +0300

    refactor code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -992,36 +992,7 @@
                         </div>
                         <!--new product area end-->
 
-                        <!--brand  area start-->
-                        <div class="brand_area brand_six mb-50">
-                            <div class="section_title section_title_two">
-                                <h2>Logo  Brands</h2>
-                            </div>
-                            <div class="brand_carousel  brand_column4 owl-carousel">
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand.png" alt=""></a>
-                                </div>
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand1.png" alt=""></a>
-                                </div>
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand2.png" alt=""></a>
-                                </div>
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand3.png" alt=""></a>
-                                </div>
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand4.png" alt=""></a>
-                                </div>
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand5.png" alt=""></a>
-                                </div>
-                                <div class="single_brand">
-                                    <a href="#"><img src="assets/frontend/img/brand/brand1.png" alt=""></a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--brand area end-->
+
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-12">
@@ -1357,28 +1328,6 @@
         </div>
     </section>
     <!--home section five end-->
-
-    <!--brand newsletter area start-->
-    <div class="newsletter_six_area">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="newsletter_inner">
-                        <div class="newsletter_content">
-                            <h2>SUBSCRIBE TO OUR NEWSLETTER</h2>
-                        </div>
-                        <div class="newsletter_form">
-                            <form action="#">
-                                <input placeholder="Email..." type="text">
-                                <button type="submit"><i class="zmdi zmdi-mail-send"></i></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--brand area end-->
 
 
 
