9b5ddc99a292852066d2b570a6f2e3a70ebc0896
wandieinnocents <wandieinnocent2@gmail.com>
2023-05-20 23:55:10 +0300

    Refactor code

--- resources/views/backend/pages_backend/products/create.blade.php
+++ resources/views/backend/pages_backend/products/create.blade.php
@@ -9,7 +9,7 @@ Add Products
 <!-- Main content dashboard  -->
 <div class="page-content">
                     <div class="container-fluid">
-                        <!-- start page title -->
+                        <!-- start page title here -->
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
